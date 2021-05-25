c1cefef8a8f23b7250f978c1c872561c88852e16
Elija de Hoog <elija.dehoog@gmail.com>
2021-05-25 12:56:51 -0700

    Adjusted OP_Char table creation script to check if the table exists and if it does not it will be created.

--- SQL_Code/StoredProcs/Tables/OP_Char.sql
+++ SQL_Code/StoredProcs/Tables/OP_Char.sql
@@ -1,11 +1,18 @@
 USE [OGMA_Test]
 GO
 
-CREATE TABLE [dbo].[OP_Char](
-    char_id int,
-    person_id int,
-    char_type varchar(64), -- 'DashboardExt' or 'ReportExt'
-    char_type_qualifier varchar(64), -- Pointer
-    char_value_text varchar(max),
-)
+IF OBJECT_ID('dbo.OP_Char') IS NOT NULL
+	BEGIN
+		PRINT 'OP_Char Table Exists'
+	END
+ELSE
+	BEGIN
+		CREATE TABLE [dbo].[OP_Char](
+			char_id int,
+			person_id int,
+			char_type varchar(64), -- 'DashboardExt' or 'ReportExt'
+			char_type_qualifier varchar(64), -- Pointer
+			char_value_text varchar(max),
+		)
+	END
 GO

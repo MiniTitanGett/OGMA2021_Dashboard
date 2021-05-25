21940bf5b0a2e94c2a25721bb7f36277ef737097
Derrick Chow <derrickcchow2012@gmail.com>
2021-05-25 15:14:43 -0700

    changed startdata from bulk insert to insert statement and made a check to see if the tables already exists

--- SQL_Code/StoredProcs/Tables/OP_reftable.sql
+++ SQL_Code/StoredProcs/Tables/OP_reftable.sql
@@ -2,21 +2,24 @@ USE [OGMA_Test]
 GO
 
 /****** Object:  Table [dbo].[OP_Ref]    Script Date: 2021-05-19 10:29:31 AM ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
+IF OBJECT_ID('[dbo].[OP_Ref]') IS NOT NULL
+    BEGIN
+        PRINT'OP_Ref Table Exists'
+    END
 
-CREATE TABLE [dbo].[OP_Ref](
-	ref_id int unique NOT NULL IDENTITY(1,1) ,
-	ref_table varchar(64) NOT NULL ,
-	ref_value varchar(64) NOT NULL ,
-	language varchar(20) NOT NULL ,
-	ref_desc varchar(max) NOT NULL ,
-	CONSTRAINT PK_reftable PRIMARY KEY (ref_table,ref_value,language)
+ELSE
+    BEGIN
+        CREATE TABLE [dbo].[OP_Ref](
+	        ref_id int unique NOT NULL IDENTITY(1,1) ,
+	        ref_table varchar(64) NOT NULL ,
+	        ref_value varchar(64) NOT NULL ,
+	        language varchar(20) NOT NULL ,
+	        ref_desc varchar(max) NOT NULL ,
+	        CONSTRAINT PK_reftable PRIMARY KEY (ref_table,ref_value,language)
 
-) ON [PRIMARY]
+        ) ON [PRIMARY]
+    END
 GO
 
 

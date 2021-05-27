4380044df379a8b023251c242188d733899c575d
Derrick Chow <derrickcchow2012@gmail.com>
2021-05-27 09:00:54 -0700

    Fix these error: Msg 156, Level 15, State 1, Line 6 Incorrect syntax near the keyword 'PROCEDURE'. Msg 137, Level 15, State 2, Line 16 Must declare the scalar variable "@person_id". Made a work around so it execute normally now

--- SQL_Code/StoredProcs/Tables/OP_AddGetEditDeleteFind_Char.sql
+++ SQL_Code/StoredProcs/Tables/OP_AddGetEditDeleteFind_Char.sql
@@ -2,15 +2,18 @@ IF OBJECT_ID('OP_AddGetEditDeleteFind_Char') IS NOT NULL
     PRINT 'OP_AddGetEditDeleteFind_Char Procedure Exists'
 
 ELSE
-    CREATE PROCEDURE OP_AddGetEditDeleteFind_Char
+	BEGIN
+		EXEC('CREATE PROCEDURE OP_AddGetEditDeleteFind_Char
 
-	    @person_id int,
-	    @char_type varchar(20),
-	    @char_type_qualifier varchar(30)
+			@person_id int,
+			@char_type varchar(64),
+			@char_type_qualifier varchar(64)
 
-	    AS
-
-	    SELECT char_type, char_type_qualifier, char_value_text
-	    FROM [dbo].[OP_Char]
-	    where [dbo].[OP_Char].person_id=@person_id AND [dbo].[OP_Char].char_type=@char_type AND [dbo].[OP_Char].char_type_qualifier=@char_type_qualifier
+			AS
+			BEGIN
+			SELECT char_type, char_type_qualifier, char_value_text
+			FROM [dbo].[OP_Char]
+			where [dbo].[OP_Char].person_id=@person_id AND [dbo].[OP_Char].char_type=@char_type AND [dbo].[OP_Char].char_type_qualifier=@char_type_qualifier
+		END')
+	END
     GO

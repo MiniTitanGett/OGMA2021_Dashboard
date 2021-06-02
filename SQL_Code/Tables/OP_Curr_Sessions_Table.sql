213e1b0b43c3191fb8d1b3dd7bd173ade324467e
Derrick Chow <derrickcchow2012@gmail.com>
2021-06-02 10:03:40 -0700

    Reorganized the sql code folder and change ref value to include 'LBL_' in OP_Ref

---
+++ SQL_Code/Tables/OP_Curr_Sessions_Table.sql
@@ -0,0 +1,20 @@
+USE [OGMA_Test]
+GO
+
+/****** Object:  Table [dbo].[OP_Curr_Sessions]    Script Date: 2021-06-02 9:08:14 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OP_Curr_Sessions]') AND type in (N'U'))
+DROP TABLE [dbo].[OP_Curr_Sessions]
+GO
+
+/****** Object:  Table [dbo].[OP_Curr_Sessions]    Script Date: 2021-06-02 9:08:14 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[OP_Curr_Sessions](
+	[session_id] [int] NOT NULL,
+	[person_id] [int] NOT NULL
+) ON [PRIMARY]
+GO

9fe92827c293a60cd654e64951459b4ea7f74fb4
Mike Gruber <mike.gruber@ogmaconsulting.com>
2021-06-29 09:30:51 -0700

    update char_id to identity

--- SQL_Code/Tables/OP_Char.sql
+++ SQL_Code/Tables/OP_Char.sql
@@ -4,11 +4,33 @@ GO
 IF OBJECT_ID('dbo.OP_Char') IS NOT NULL
 	BEGIN
 		PRINT 'OP_Char Table Exists'
+
+    if (exists (select top 1 1 from dbo.op_char where char_id is null))
+    begin
+
+		  CREATE TABLE [dbo].[OP_Char_TEMP](
+			  char_id int unique NOT NULL IDENTITY(1,1),
+			  person_id int,
+			  char_type varchar(64), -- 'DashboardExt' or 'ReportExt'
+			  char_type_qualifier varchar(64), -- Pointer
+			  char_value_text varchar(max),
+		  )
+
+      insert dbo.op_char_temp (person_id, char_type, char_type_qualifier, char_value_text)
+      select person_id, char_type, char_type_qualifier, char_value_text
+        from dbo.op_char
+
+      drop table dbo.op_char
+
+      execute sp_rename N'dbo.OP_Char_TEMP', N'OP_Char', 'OBJECT'
+
+    end
+
 	END
 ELSE
 	BEGIN
 		CREATE TABLE [dbo].[OP_Char](
-			char_id int,
+			char_id int unique NOT NULL IDENTITY(1,1),
 			person_id int,
 			char_type varchar(64), -- 'DashboardExt' or 'ReportExt'
 			char_type_qualifier varchar(64), -- Pointer
